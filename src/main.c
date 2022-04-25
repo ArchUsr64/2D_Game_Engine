4e687f3cfeffb20b8ad32be5f49a33e73f1c1b67
ArchUsr64 <anshulusr@gmail.com>
2022-04-25 08:14:28 +0530

    Code quality

--- src/main.c
+++ src/main.c
@@ -19,11 +19,11 @@ int main() {
   pthread_t physics_thread_id;
   pthread_create(&physics_thread_id, NULL, physics_thread, NULL);
   while (!USER_QUIT) {
+    set_draw_colour(0, 0, 0, 1);
     SDL_RenderClear(RENDERER_PTR);
     render_entity(&player);
     set_draw_colour(1, 1, 1, 1);
     SDL_RenderPresent(RENDERER_PTR);
-    set_draw_colour(0, 0, 0, 1);
   }
   pthread_join(input_thread_id, NULL);
   pthread_join(physics_thread_id, NULL);
